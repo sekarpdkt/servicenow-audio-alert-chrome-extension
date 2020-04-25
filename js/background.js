c7c009d96e865f9f7024b2bca70de23db47fa4e4
sekarpdkt <sekarpdkt@gmail.com>
2020-04-25 15:22:01 +0530

    Enabled additional features
    in sometimes will be uploading some new feature
    1. You can disable alarm
    2. You can disable poll
    3. You can enable alarm on either >0 count or new ticket to that monitored queue
    4. You can set polling interval

--- js/background.js
+++ js/background.js
@@ -14,7 +14,10 @@ var type,
     $taskData,
     newStamp = 0,
     totalCount;
+let lastPollAt;
+let newList=[],oldList=[],difference=[];
 
+let myStorage = window.localStorage;
 if ($idleState == undefined) {
     $idleState = "active"
 }
@@ -40,18 +43,28 @@ chrome.runtime.onInstalled.addListener(function(details) {
     getSavedData();
 });
 
-chrome.alarms.create("CheckTicketsAlarm", {
-    delayInMinutes: 1,
-    periodInMinutes: 1
-});
+
+function createAlarm(duration){
+   
+
+    chrome.alarms.create("CheckTicketsAlarm", {
+        delayInMinutes: duration,
+        periodInMinutes: duration
+    });
+}
+createAlarm(1);
 
 chrome.alarms.onAlarm.addListener(function(info, tab) {
     getSavedData();
 });
 
-
+function resetAlarm(duration){
+    chrome.alarms.clear("CheckTicketsAlarm", function(){
+        createAlarm(duration);
+    }) 
+}
 function getSavedData() {
-    chrome.storage.sync.get(['rooturl', 'secondary', 'primary', 'splitcount'], getQueues);
+    chrome.storage.sync.get(['rooturl', 'secondary', 'primary', 'splitcount', 'disableAlarm', 'disablePoll', 'nonZeroCount', 'alarmOnNewEntry'], getQueues);
     //  chrome.storage.sync.get(null, function (data) { console.info(data) });
 
 }
@@ -68,13 +81,26 @@ function createNotification(){
     //include this line if you want to clear the notification after 5 seconds
     setTimeout(function(){chrome.notifications.clear("notificationName",function(){});},5000);
 }
-
+function getlastPollAt() {
+    return     myStorage.getItem('lastPollAt')
+;
+}
 
 function getQueues(items) {
-    var primaryURL = changeURLforGetXML(items.primary),
-        secondaryURL = changeURLforGetXML(items.secondary),
+    if(items.disablePoll=="on"){
+        oldList=[];
+        difference=[];
+        newList=[];
+        return;
+    }
+    if(items.nonZeroCount == null && items.alarmOnNewEntry==null)
+        items.nonZeroCount ="on";
+    var primaryURL = changeURLforRESTAPI(items.primary),
+        secondaryURL = changeURLforRESTAPI(items.secondary),
         count = 0;
-				$rootURL = items.rooturl;
+        $rootURL = items.rooturl;
+        newList=[];
+    
     if (primaryURL != undefined && primaryURL != "") {
         count += 1
     }
@@ -84,31 +110,31 @@ function getQueues(items) {
     if (count > 0) {
         if (count == 1) {
 
-            var data = getData(primaryURL);
+            var data = getDataREST(primaryURL);
+            
             totalCount = data['quantity'];
             if ($currentNumberTotal < totalCount) {
                 $ticketNumberGlobal = data['number'];
-                audioNotification();
                 showNotification(data['number'], data['description'], data['severity'])
             }
-
             chrome.browserAction.setBadgeText({
                 text: ((data['quantity'])).toString()
             });
             $currentNumberTotal = totalCount
-
+                chrome.browserAction.setBadgeText({
+                    text: (totalCount).toString()
+                });
 
             // handle request for 1 field
         } else if (count == 2) {
-            var data1 = getData(primaryURL);
-            var data2 = getData(secondaryURL);
+            var data1 = getDataREST(primaryURL);
+            var data2 = getDataREST(secondaryURL);
             totalCount = (data1['quantity'] + data2['quantity']);
             if ($currentNumberTotal < totalCount) {
                 if (data1.timestamp > data2.timestamp) {
                     if (data1.timestamp > newStamp) {
                         newStamp = data1.timestamp;
                         $ticketNumberGlobal = data1['number'];
-                        audioNotification();
                         showNotification(data1['number'], data1['description'], data1['severity'])
                         if (items.splitcount == "true") {
                             chrome.browserAction.setBadgeText({
@@ -124,8 +150,6 @@ function getQueues(items) {
                     if (data2.timestamp > newStamp) {
                         newStamp = data2.timestamp;
                         $ticketNumberGlobal = data2['number'];
-                        audioNotification();
-
                         showNotification(data2['number'], data2['description'], data2['severity'])
 
                     }
@@ -144,12 +168,34 @@ function getQueues(items) {
             
             //handle request for 2 fields
         }
+
+
+        if (totalCount >0 && items.nonZeroCount=="on") {
+            $ticketNumberGlobal = data['number'];
+            if(items.disableAlarm=="on")
+                audioNotification();
+        }
+        else{
+            difference = newList.filter(x => !oldList.includes(x));
+            if(difference.length>0){
+                $ticketNumberGlobal = data['number'];
+                if(items.disableAlarm=="on")
+                    audioNotification();
+            }
+        }
+        oldList=[]
+        difference=[]
+        oldList=oldList.concat(newList)
+        myStorage.setItem('lastPollAt', new Date());
+        
+        
     }
 }
 
 
 
-function getData(url) {
+
+function getDataREST(url) {
     var $BadgeCount
     var timestamps = [];
     var oldest = [];
@@ -158,7 +204,7 @@ function getData(url) {
         type: "get",
         url: url,
         async: false,
-        dataType: "xml",
+        dataType: "json",
         success: function(data) {
             $queueData = $(data);
         },
@@ -168,20 +214,21 @@ function getData(url) {
             });
         }
     })
-    var $qTickets = $queueData.children().children().length;
+    var $qTickets = $queueData[0].result.length;
     var max = 0;
     if ($qTickets > 0) {
-        $queueData.children().children().each(function() {
-            var $qticketNumber = $(this).find('number').text();
+        $queueData[0].result.forEach(function(record) {
+            var $qticketNumber = record.number;
+            newList.push($qticketNumber)
             if ($qticketNumber.indexOf("TASK") != -1) {
                 var $qseverity = "10";
             } else if ($qticketNumber.indexOf("CHG") != -1) {
                 var $qseverity = "15";
             } else {
-                var $qseverity = $(this).find("priority").text();
+                var $qseverity = record.priority;
             }
-            var $qticketDescription = $(this).find("short_description").text();
-            var time = $(this).find("sys_updated_on").text();
+            var $qticketDescription = record.short_description;
+            var time =record.sys_updated_on;
             var d1 = new Date(time);
             var qtime = parseInt(d1.getTime());
             var dataOutput1 = {
@@ -205,13 +252,13 @@ function getData(url) {
             "description": null,
             "timestamp": 0
         }
-				dataOutput = dataOutput1;
+        
+        dataOutput = dataOutput1;
 
     }
     var output = Math.max(oldest);
     return dataOutput;
 }
-
 
 
 
@@ -309,18 +356,64 @@ function hasValue(item) {
 
 
 
-function changeURLforGetXML(url) {
-    index = url.indexOf("?")
-    if (index == -1) {
-        return undefined;
+
+function removeParam(key, sourceURL) {
+    var rtn = sourceURL.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === key) {
+                params_arr.splice(i, 1);
+            }
+        }
+        rtn = rtn + "?" + params_arr.join("&");
     }
-    return url.slice(0, index + 1) + "XML&" + url.slice(index + 1, url.length)
+    return rtn;
 }
 
 
+function changeURLforRESTAPI(url) {
 
+    if(url=="" || url==undefined)
+        return undefined;
+    // Created a URL object using URL() method 
+    var parser = new URL(url); 
+        
+    // Protocol used in URL 
+    let protocol=(parser.protocol); 
+    
+    // Host of the URL 
+    let host=(parser.host); 
+    
+    // Port in the URL 
+    let port=(parser.port); 
+    
+    // Hostname of the URL 
+    let pathname=(parser.pathname) 
 
+    // Search in the URL 
+    let search=(parser.search); 
+        
+    /*
+     * Note: Port will be included in host. So while joining back, dont include port
+     */
 
+    if(protocol!="")
+        protocol=protocol+"//";
+    pathname="/api/now/table"+pathname.replace("_list.do","");
+    
+    // Search parameter in the URL 
+    let restURL=`${protocol}//${host}${pathname}${search}`;
+
+    restURL=removeParam("sysparm_fields",restURL)
+    restURL=removeParam("sysparm_view",restURL)
+    restURL=restURL+"&sysparm_fields=number%2Cseverity%2Cshort_description%2Cpriority%2Csys_id%2Csys_updated_on";
+    return restURL;
+}
 
 
 chrome.idle.onStateChanged.addListener(function(state) {
--- js/options.js
+++ js/options.js
@@ -42,60 +42,40 @@ function save_options() {
     var primary = $("#idprimaryq").val()
     var rooturl = $("#idrooturl").val()
     var secondary = $("#idsecondaryq").val()
+    var pollInterval=parseInt($("#pollInterval").val());
+    if(pollInterval<1 || pollInterval==undefined) 
+        pollInterval=5;
+    if (isNaN(pollInterval)) 
+        pollInterval=5;
     var $followWorkflow
 		var saveMe = {
 			'primary': primary,
 			'rooturl': rooturl,
 			'secondary': secondary,
+            'pollInterval':pollInterval,
       'splitcount' : $("input[name='splitcount']:checked").val(),
-      'q1name' : $("#q1name").val(),
-      'q1url' : $("#q1url").val(),
-      'q2name' : $("#q2name").val(),
-      'q2url' : $("#q2url").val(),
-      'q3name' : $("#q3name").val(),
-      'q3url' : $("#q3url").val(),
-      'q4name' : $("#q4name").val(),
-      'q4url' : $("#q4url").val(),
-      'l1name' : $("#l1name").val(),
-      'l1url' : $("#l1url").val(),
-      'l2name' : $("#l2name").val(),
-      'l2url' : $("#l2url").val(),
-      'l3name' : $("#l3name").val(),
-      'l3url' : $("#l3url").val(),
-      'l4name' : $("#l4name").val(),
-      'l4url' : $("#l4url").val(),
-      'create' : false,
-      'call' : false,
-      'incident' : false,
-      'change' : false,
-      'request' : false,
-      'search' : false,
-      'queues' : false,
-      'queue1' : false,
-      'queue2' : false,
-      'queue3' : false,
-      'queue4' : false,
-      'lists' : false,
-      'list1' : false,
-      'list2' : false,
-      'list3' : false,
-      'list4' : false
+      'disableAlarm' : $("input[name='disableAlarm']:checked").val(),
+      'disablePoll' : $("input[name='disablePoll']:checked").val(),
+      'nonZeroCount' : $("input[name='nonZeroCount']:checked").val(),
+      'alarmOnNewEntry' : $("input[name='alarmOnNewEntry']:checked").val()
 		}
 
 
-		var options = [];
-    $.each($("input[name='option']:checked"), function() {
-        options.push($(this).val());
-				var needle = $(this).val();
-						saveMe[needle] = true;
-    });
+    if(saveMe.nonZeroCount == saveMe.alarmOnNewEntry){
+        $("#nonZeroCount").attr("checked", true);
+        $("#alarmOnNewEntry").attr("checked", false);
+        saveMe.nonZeroCount ="on";
+        saveMe.alarmOnNewEntry =null;
+    }
 
     chrome.storage.sync.set(saveMe, function() {
         showSuccessMessage("Options saved!")
+        chrome.storage.sync.get(null, function (data) { 
+            console.info(data) 
+        });
+        getSavedData();
     });
     window.scrollTo(0,0);
-    chrome.storage.sync.get(null, function (data) { console.info(data) });
-    getSavedData();
 }
 
 function isEmpty(value) {
@@ -131,6 +111,7 @@ function clear_options() {
     chrome.storage.sync.clear();
     $("input").val("");
     $(".check").attr("checked", false);
+    $("#nonZeroCount").attr("checked", true);
 
     var status = document.getElementById('status');
     status.textContent = 'Options erased';
@@ -148,88 +129,64 @@ function restore_options() {
     chrome.storage.sync.get(['rooturl',
                              'primary',
                              'secondary',
-                             'create',
-                             'call',
-                             'incident',
-                             'change',
-                             'request',
-                             'search',
-                             'queues',
-                             'queue1',
-                             'queue2',
-                             'queue3',
-                             'queue4',
-                             'lists',
-                             'list1',
-                             'list2',
-                             'list3',
-                             'list4',
-                             'q1name',
-                             'q1url',
-                             'q2name',
-                             'q2url',
-                             'q3name',
-                             'q3url',
-                             'q4name',
-                             'q4url',
-                             'l1name',
-                             'l1url',
-                             'l2name',
-                             'l2url',
-                             'l3name',
-                             'l3url',
-                             'l4name',
-                             'l4url',
+                             'disableAlarm',
+                             'disablePoll',
+                             'pollInterval',
+                             'alarmOnNewEntry',
+                             'nonZeroCount',
                              'splitcount'], function(items) {
         if(items.splitcount == "true"){
           $("#splitcounttrue").attr("checked", true);
         }else {
           $("#splitcountfalse").attr("checked", true);
         }
+
+        if(items.disableAlarm == "on"){
+          $("#disableAlarm").attr("checked", true);
+        }
+        
+        
+        if(items.disablePoll == "on"){
+          $("#disablePoll").attr("checked", true);
+        }
+        
+        if(items.alarmOnNewEntry == "on"){
+          $("#alarmOnNewEntry").attr("checked", true);
+        }
+        
+        if(items.nonZeroCount == "on"){
+          $("#nonZeroCount").attr("checked", true);
+        }
+        
+        var pollInterval=parseInt(items.pollInterval);
+        if(pollInterval<1 || pollInterval==undefined) 
+            pollInterval=5;
+        if (isNaN(pollInterval)) 
+            pollInterval=5;
+
+        $("#pollInterval").val(pollInterval);
         $("#idrooturl").val(items.rooturl);
         $("#idsecondaryq").val(items.secondary);
         $("#idprimaryq").val(items.primary);
-        $("#q1name").val(items.q1name),
-        $("#q1url").val(items.q1url),
-        $("#q2name").val(items.q2name),
-        $("#q2url").val(items.q2url),
-        $("#q3name").val(items.q3name),
-        $("#q3url").val(items.q3url),
-        $("#q4name").val(items.q4name),
-        $("#q4url").val(items.q4url),
-        $("#l1name").val(items.l1name),
-        $("#l1url").val(items.l1url),
-        $("#l2name").val(items.l2name),
-        $("#l2url").val(items.l2url),
-        $("#l3name").val(items.l3name),
-        $("#l3url").val(items.l3url),
-        $("#l4name").val(items.l4name),
-        $("#l4url").val(items.l4url),
-        $.each(items, function (key, value) {
-          if (value === true){
-            if(key == "create"){
-              $("#" + key).attr("checked", true);
-              $(".myHide1").toggle();
-            }else if (key == "queues") {
-              $("#" + key).attr("checked", true);
-              $(".myHide2").toggle();
-            }else if (key == "lists") {
-              $("#" + key).attr("checked", true);
-              $(".myHide3").toggle();
-            }
-          $("#" + key).attr("checked", true);
-          $("." + key).toggle();
-          }
-
-        })
-
     });
 }
 
 $(document).ready(function() {
     $("#status").hide()
     restore_options();
+    $("#lastPollAt").text(getlastPollAt());
 
+    $("#idprimaryq").focusout(function(event) {
+                $("#save").click();
+        })
+
+    $("#idrooturl").focusout(function(event) {
+                $("#save").click();
+        })
+
+    $("#idsecondaryq").focusout(function(event) {
+                $("#save").click();
+        })
 
     $("#idprimaryq").keyup(function(event) {
         if (event.keyCode == 13) {
